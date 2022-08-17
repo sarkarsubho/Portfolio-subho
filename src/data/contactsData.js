ebfe38e3c53309d95357b82de28ba258c312e9ac
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-08-18 00:29:56 +0530

    commit sheet api

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,5 +3,5 @@ export const contactsData = {
     phone: '7031872272',
     address: 'Durgapur,WestBangal',
 
-    sheetAPI: 'https://api.emailjs.com/api/v1.0/email/send'
+    sheetAPI: 'https://api.emailjs.com/api/v1.0/email/send-form'
 }
