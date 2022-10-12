7227602f095ccef72707dba4d0fe87064cb41bc1
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-10-13 00:16:03 +0530

    updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: `Passionate Full Stack Web Developer with a specialization in MERN stack. Skilled, collaborative and attentive developer with a drive to develop the most relevant products with the ability to write clean and reusable code.Have ability to perform well in a team. Always looking forward to learning something new to strengthen skills learned.`,
+    description1: `Passionate Full Stack Web Developer with a specialization in MERN stack. Skilled, collaborative and attentive developer with a drive to develop the most relevant products with the ability to write clean and reusable code. Have ability to perform well in a team. Always looking forward to learning something new to strengthen skills learned.`,
     description2: "",
     image: 2
 }
