e68106586a7060d5feddeb3b0f5ff6ab928d7fc4
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-12-23 03:20:39 +0530

    project added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -45,9 +45,25 @@ export const projectsData = [
     demo: "https://media-net.vercel.app/",
     image: "https://miro.medium.com/max/720/1*_2rbduDnu1SzRDjNr3Dc8A.webp",
   },
-
   {
     id: 4,
+    projectName: "E-commerce App",
+    projectDesc:"A basic e-commerse app where a user can register and login and can add-review and add and delete products in cart.",
+    tags: ["React", "Redux", "redux-thunk", "Chakra-ui"],
+    logos: [
+      "fab fa-react",
+      "redux",
+      "fab fa-js",
+      "fab fa-html5",
+      "fab fa-css3",
+    ],
+    code: "https://github.com/sarkarsubho/E-commerce-app.",
+    demo: "https://e-commerce-app-green.vercel.app/",
+    image: "https://camo.githubusercontent.com/e142a159a6ae8b1296d058ad7885efdda7c5ce843699c567a298391162868856/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f3732302f312a596534716d6d4974326d4653534975437934655266412e706e67",
+  },
+
+  {
+    id: 5,
     projectName: "Deyga- Clone",
     projectDesc:
       "This is a Indian Natural Cosmetics & Beauty products selling website. this clone can login a user with email and on the product page user can sort the product and select any inthe cart.",
@@ -64,7 +80,7 @@ export const projectsData = [
     image: "https://miro.medium.com/max/700/1*hOqavOAFqBGQiuQLr_PO4Q.png",
   },
   {
-    id: 5,
+    id: 6,
     projectName: "Project Zoomcar",
     projectDesc:
       "Zoomcar, is a very populan Indian self-drive car rental company, headquartered in Bangalore, India. This Project is assigned to us by masai during the construct week.",
@@ -75,7 +91,7 @@ export const projectsData = [
     image: "https://miro.medium.com/max/700/0*dRRi7mwcDE1rOt9e.png",
   },
   {
-    id: 6,
+    id: 7,
     projectName: "Youtube-copy",
     projectDesc:
       "It is a small Copy of Youtube .as a assigment in masai school. this copy alows the user to search for any type of vedio onclicking on the particular vedio on the page and show it on the other page.  ",
@@ -87,7 +103,7 @@ export const projectsData = [
       "https://github.com/sarkarsubho/Youtube-Clone/raw/main/Screenshot%202022-04-29%20134544.png",
   },
   {
-    id: 7,
+    id: 8,
     projectName: "Unplash",
     projectDesc:
       "it is a simple copy of unplash., This clone allows users to search for any kind of images and show it in the another page",
@@ -97,17 +113,7 @@ export const projectsData = [
     demo: "https://sensational-unplash.netlify.app/",
     image:
       "https://github.com/sarkarsubho/unplash-Clone/raw/main/Screenshot%202022-04-30%20172424.png",
-  },
-  // {
-  //     id: 6,
-  //     projectName: 'Project MightyCause',
-  //     projectDesc: 'It is a online fundraising website for empowering nonprofits donations. We tried to clone the Mightycause website from the scratch. A user can create his/her porfile and create any number of events they want from their dashboard.',
-  //     tags: ["React.js", "material.ui"],
-  //      logos: ["fab fa-react", "fas fa-database", "fab fa-js"],
-  //     code: 'https://github.com/cybervirus997/Mightycause',
-  //     demo: 'https://mighty-cause-ravishukla86044.vercel.app/',
-  //     image: "https://user-images.githubusercontent.com/77038954/129859566-716f10fe-7b02-465f-962e-981da3632e85.png"
-  // },
+  }
 ];
 
 //schema for entwreing data
