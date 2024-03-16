c267b6599806b0cfc7589b2452f06ceb54e5cef0
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-03-16 12:06:54 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,8 +3,8 @@ export const experienceData = [
         id: 1,
         company: "Masai school" ,
         jobtitle: 'FullStack Developer (Problem Setter)',
-        startYear: '2023-Feb',
-         endYear: '2024-Feb'
+        startYear: '2023-February',
+         endYear: '2024-February'
     },
    
 ]
